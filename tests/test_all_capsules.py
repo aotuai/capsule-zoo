97b8cdd5222417816bd82c85f5ef2f25e475d4e1
Alex <alex.thiel@aotu.ai>
2020-05-08 12:26:39 -0700

    Added a check for README and made the test a bit more generic.

--- tests/test_all_capsules.py
+++ tests/test_all_capsules.py
@@ -30,14 +30,10 @@ def test_capsules(unpackaged_capsule_dir: Path):
     argnames=["unpackaged_capsule_dir"],
     argvalues=capsule_paths_argvals,
     ids=capsule_paths)
-def test_capsules_licensing(unpackaged_capsule_dir: Path):
-    """Test that each capsule has a licenses/ directory, and at least has a
-    code.LICENSE file. To catch mistakes, it also checks if there are common
-    model formats (*.pb, *.bin, *.tflite) and requires that a model.LICENSE
-    be included in the capsule as well.
-
-    If we encounter a capsule that uses these file extensions but doesn't
-    actually need them, we can whitelist it."""
+def test_capsules_required_information_files(unpackaged_capsule_dir: Path):
+    """Test that each capsule has a certain required files for humans.
+    All required runtime files are tested by test_capsules.
+    """
 
     # Verify a licenses directory exists
     licenses_dir = unpackaged_capsule_dir / "licenses"
@@ -47,6 +43,10 @@ def test_capsules_licensing(unpackaged_capsule_dir: Path):
     code_license = licenses_dir / "code.LICENSE"
     assert code_license.is_file()
 
+    # Verify there is a README.md
+    readme_file = unpackaged_capsule_dir / "README.md"
+    assert readme_file.is_file()
+
     # Check if there are model files and verify there is a model license file
     filetypes = ("*.pb", "*.tflite", "*.bin", "*.h5")
     globs = [list(unpackaged_capsule_dir.glob(ftype)) for ftype in filetypes]
@@ -54,4 +54,3 @@ def test_capsules_licensing(unpackaged_capsule_dir: Path):
     if len(model_files):
         model_license = licenses_dir / "model.LICENSE"
         assert model_license.is_file()
-
