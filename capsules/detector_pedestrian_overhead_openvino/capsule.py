7291af8ff380f6ae276807ccbd29ca8f2654becc
Alex <alex.thiel@aotu.ai>
2020-05-07 18:58:01 -0700

    Added the detector_pedestrian_overhead_openvino capsule.
    Co-authored-by: Bryce Beagle <bryce.beagle@gmail.com>
    Co-authored-by: Tyler Compton <xaviosx@gmail.com>

---
+++ capsules/detector_pedestrian_overhead_openvino/capsule.py
@@ -0,0 +1,24 @@
+from vcap import (
+    BaseCapsule,
+    NodeDescription,
+    DeviceMapper,
+    common_detector_options
+)
+from .backend import Backend
+
+
+class Capsule(BaseCapsule):
+    name = "detector_pedestrian_overhead_openvino"
+    description = "CPU-only fast person detector. Works best in surveillance " \
+                  "perspectives from a downwards facing point of view."
+    version = 1
+    device_mapper = DeviceMapper.map_to_single_cpu()
+    input_type = NodeDescription(size=NodeDescription.Size.NONE)
+    output_type = NodeDescription(
+        size=NodeDescription.Size.ALL,
+        detections=["person"])
+    backend_loader = lambda capsule_files, device: Backend.from_bytes(
+        model_bytes=capsule_files["person-detection-retail-0013.xml"],
+        weights_bytes=capsule_files["person-detection-retail-0013.bin"],
+    )
+    options = common_detector_options
