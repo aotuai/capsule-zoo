5dc6c23ebd0316298d458d9d0ee9fee14027d826
Tyler Compton <xaviosx@gmail.com>
2020-07-10 13:21:47 -0700

    Add new person, vehicle, & bike detector capsule

---
+++ capsules/detector_person_vehicle_bike_openvino/capsule.py
@@ -0,0 +1,25 @@
+from vcap import (
+    BaseCapsule,
+    NodeDescription,
+    DeviceMapper,
+    common_detector_options
+)
+from .backend import Backend
+
+
+class Capsule(BaseCapsule):
+    name = "detector_person_vehicle_bike_openvino"
+    description = ("OpenVINO person, vehicle, and bike detector. Optimized "
+                   "for surveillance camera scenarios.")
+    version = 1
+    device_mapper = DeviceMapper.map_to_all_myriad()
+    input_type = NodeDescription(size=NodeDescription.Size.NONE)
+    output_type = NodeDescription(
+       size=NodeDescription.Size.ALL,
+       detections=["vehicle", "person", "bike"])
+    backend_loader = lambda capsule_files, device: Backend(
+       model_xml=capsule_files["person-vehicle-bike-detection-crossroad-1016-fp32.xml"],
+       weights_bin=capsule_files["person-vehicle-bike-detection-crossroad-1016-fp32.bin"],
+       device_name=device
+    )
+    options = common_detector_options
