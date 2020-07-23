e1af275670d77d62005e57ab3229f0fb2abca25c
Alex <alex.thiel@aotu.ai>
2020-07-23 12:24:48 -0700

    Removed usage of BackendRpcProcess after benchmarks show it caps performance at 80 FPS

--- capsules/detector_person_vehicle_bike_openvino/capsule.py
+++ capsules/detector_person_vehicle_bike_openvino/capsule.py
@@ -4,7 +4,6 @@ from vcap import (
     DeviceMapper,
     common_detector_options
 )
-from vcap_utils import BackendRpcProcess
 from .backend import Backend
 
 
@@ -18,8 +17,7 @@ class Capsule(BaseCapsule):
     output_type = NodeDescription(
        size=NodeDescription.Size.ALL,
        detections=["vehicle", "person", "bike"])
-    backend_loader = lambda capsule_files, device: BackendRpcProcess(
-       Backend,
+    backend_loader = lambda capsule_files, device: Backend(
        model_xml=capsule_files["person-vehicle-bike-detection-crossroad-1016-fp32.xml"],
        weights_bin=capsule_files["person-vehicle-bike-detection-crossroad-1016-fp32.bin"],
        device_name=device
