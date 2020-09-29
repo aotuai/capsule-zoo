ee96a0f5cbee7b9833dad833789977020a59b11b
Ryan Hedgecock <rhedgeco@gmail.com>
2020-09-29 15:00:03 -0700

    removed redundant string literal and updated description

--- capsules/detector_text_openvino/capsule.py
+++ capsules/detector_text_openvino/capsule.py
@@ -10,35 +10,35 @@ from .backend import Backend, OpenVINOModel
 
 class Capsule(BaseCapsule):
     name = "detector_text_openvino"
-    description = "✨ OpenVINO text detector."
+    description = "✨ OpenVINO text detector and reader."
     version = 1
     device_mapper = DeviceMapper.map_to_openvino_devices()
     input_type = NodeDescription(size=NodeDescription.Size.NONE)
     output_type = NodeDescription(
         size=NodeDescription.Size.ALL,
         detections=["text"],
-        extra_data=["detection_confidence"])
+        extra_data=["detection_confidence", "text"])
 
     backend_loader = lambda capsule_files, device: Backend(
         detector=OpenVINOModel(
             model_xml=capsule_files[
-                str("models/text-spotting-0001-detector.xml")],
+                "models/text-spotting-0001-detector.xml"],
             weights_bin=capsule_files[
-                str("models/text-spotting-0001-detector.bin")],
+                "models/text-spotting-0001-detector.bin"],
             device_name=device
         ),
         recognizer_encoder=OpenVINOModel(
             model_xml=capsule_files[
-                str("models/text-spotting-0001-recognizer-encoder.xml")],
+                "models/text-spotting-0001-recognizer-encoder.xml"],
             weights_bin=capsule_files[
-                str("models/text-spotting-0001-recognizer-encoder.bin")],
+                "models/text-spotting-0001-recognizer-encoder.bin"],
             device_name=device
         ),
         recognizer_decoder=OpenVINOModel(
             model_xml=capsule_files[
-                str("models/text-spotting-0001-recognizer-decoder.xml")],
+                "models/text-spotting-0001-recognizer-decoder.xml"],
             weights_bin=capsule_files[
-                str("models/text-spotting-0001-recognizer-decoder.bin")],
+                "models/text-spotting-0001-recognizer-decoder.bin"],
             device_name=device
         )
     )
