d4ea9a9356a33f3e6e40ee621bea73bfabb71d59
Ryan Hedgecock <rhedgeco@gmail.com>
2020-10-06 13:58:39 -0700

    Added classifier_face_emotion_openvino (#5)
    * Added classifier_face_emotion_openvino
    
    * added emotion score and removed config file
    
    * fixed typo and added newline for .conf

---
+++ capsules/classifier_face_emotion_openvino/capsule.py
@@ -0,0 +1,27 @@
+from vcap import BaseCapsule, NodeDescription, DeviceMapper
+from vcap_utils import BackendRpcProcess
+from .backend import Backend, EMOTION_TYPES
+
+
+class Capsule(BaseCapsule):
+    name = "classifier_face_emotion_openvino"
+    description = "OpenVINO face emotion classifier."
+    version = 1
+    device_mapper = DeviceMapper.map_to_openvino_devices()
+    input_type = NodeDescription(
+        size=NodeDescription.Size.SINGLE,
+        detections=["face"])
+    output_type = NodeDescription(
+        size=NodeDescription.Size.SINGLE,
+        detections=["face"],
+        attributes={"emotion": EMOTION_TYPES},
+        extra_data=["emotion_confidence"]
+    )
+    backend_loader = lambda capsule_files, device: BackendRpcProcess(
+        Backend,
+        model_xml=capsule_files[
+            "emotions-recognition-retail-0003.xml"],
+        weights_bin=capsule_files[
+            "emotions-recognition-retail-0003.bin"],
+        device_name=device
+    )
