a7ff0440a389ac1352dbaac38924462fd22d7152
Alex <alex.thiel@aotu.ai>
2020-12-02 17:38:26 -0800

    Added face mask detection capsule

---
+++ capsules/classifier_mask_closeup/backend.py
@@ -0,0 +1,31 @@
+from typing import Dict
+
+import numpy as np
+
+from vcap import (
+    Resize,
+    DETECTION_NODE_TYPE,
+    OPTION_TYPE,
+    BaseStreamState)
+from vcap_utils import BaseOpenVINOBackend
+
+
+class Backend(BaseOpenVINOBackend):
+    LABELS = ["not_wearing_mask", "wearing_mask"]
+
+    def process_frame(self, frame: np.ndarray,
+                      detection_node: DETECTION_NODE_TYPE,
+                      options: Dict[str, OPTION_TYPE],
+                      state: BaseStreamState) -> DETECTION_NODE_TYPE:
+        crop = Resize(frame).crop_bbox(detection_node.bbox).frame
+
+        input_dict, _ = self.prepare_inputs(crop)
+        prediction = self.send_to_batch(input_dict).get()
+
+        # Convert prediction to a label
+        probability = prediction["fc5"].flatten()[0]
+        threshold = options["threshold"]
+        label = self.LABELS[int(probability > threshold)]
+
+        detection_node.attributes["mask"] = label
+        detection_node.extra_data["mask_confidence"] = float(probability)
---
+++ capsules/classifier_mask_closeup/capsule.py
@@ -0,0 +1,33 @@
+from vcap import BaseCapsule, NodeDescription, DeviceMapper, FloatOption
+from vcap_utils import BackendRpcProcess
+from .backend import Backend
+
+
+class Capsule(BaseCapsule):
+    name = "classifier_mask_openvino"
+    description = "OpenVINO face mask classifier."
+    version = 1
+    device_mapper = DeviceMapper.map_to_openvino_devices()
+    input_type = NodeDescription(
+        size=NodeDescription.Size.SINGLE,
+        detections=["face"])
+    output_type = NodeDescription(
+        size=NodeDescription.Size.SINGLE,
+        detections=["face"],
+        attributes={"mask": Backend.LABELS},
+        extra_data=["mask_confidence"])
+    backend_loader = lambda capsule_files, device: BackendRpcProcess(
+        Backend,
+        model_xml=capsule_files["face_mask.xml"],
+        weights_bin=capsule_files["face_mask.bin"],
+        device_name=device
+    )
+    options = {
+        "threshold": FloatOption(
+            description="Scores under this value are deemed to not be not "
+                        "wearing a mask.",
+            default=0.3,
+            min_val=None,
+            max_val=None
+        )
+    }
