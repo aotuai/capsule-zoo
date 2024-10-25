5c5d6016864b22cb381982930fea964a929cecad
Stephen Li <stephen@aotu.ai>
2024-10-24 18:02:52 -0700

    classifier_vehicle_color_openvino: support both openvino2024 and openvino2021

--- capsules/classifier_vehicle_color_openvino/backend.py
+++ capsules/classifier_vehicle_color_openvino/backend.py
@@ -8,6 +8,14 @@ from vcap import (
     OPTION_TYPE,
     BaseStreamState)
 from vcap_utils import BaseOpenVINOBackend
+from vcap import __version__ as vcap_version
+def openvino2024_compatible():
+    from packaging import version
+    from vcap import __version__ as vcap_version
+    openvino2024_compatible_vcap_version = '0.3.8'
+    return version.parse(vcap_version) >= version.parse(openvino2024_compatible_vcap_version)
+__openvino2024__ = openvino2024_compatible()
+
 from . import config
 
 
@@ -22,20 +30,24 @@ class Backend(BaseOpenVINOBackend):
         input_dict, _ = self.prepare_inputs(crop)
         prediction = self.send_to_batch(input_dict).result()
 
-        vcolor_key = next(key for key in prediction.keys() if 'color' in key.names)
-        vtype_key = next(key for key in prediction.keys() if 'type' in key.names)
+        if __openvino2024__:
+            vcolor_key = next(key for key in prediction.keys() if 'color' in key.names)
+            vtype_key = next(key for key in prediction.keys() if 'type' in key.names)
 
-        vcolor_data = prediction[vcolor_key]
-        if isinstance(vcolor_data, np.ndarray):
-            max_color = config.colors[vcolor_data.flatten().argmax()]
-        else:
-            max_color = config.colors[vcolor_data.argmax()]
+            vcolor_data = prediction[vcolor_key]
+            if isinstance(vcolor_data, np.ndarray):
+                max_color = config.colors[vcolor_data.flatten().argmax()]
+            else:
+                max_color = config.colors[vcolor_data.argmax()]
 
-        vtype_data = prediction[vtype_key]
-        if isinstance(vtype_data, np.ndarray):
-            max_type = config.vehicle_classifications[vtype_data.flatten().argmax()]
+            vtype_data = prediction[vtype_key]
+            if isinstance(vtype_data, np.ndarray):
+                max_type = config.vehicle_classifications[vtype_data.flatten().argmax()]
+            else:
+                max_type = config.vehicle_classifications[vtype_data.argmax()]
         else:
-            max_type = config.vehicle_classifications[vtype_data.argmax()]
+            max_color = config.colors[prediction["color"].argmax()]
+            max_type = config.vehicle_classifications[prediction["type"].argmax()]
 
         detection_node.attributes["color"] = max_color
         detection_node.attributes["vehicle_type"] = max_type
