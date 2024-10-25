623024e961688630f28c426e0960b3d1b99d20eb
Stephen Li <stephen@aotu.ai>
2024-10-24 18:02:17 -0700

    classifier_face_emotion_openvino: support both openvino2024 and openvino2021

--- capsules/classifier_face_emotion_openvino/backend.py
+++ capsules/classifier_face_emotion_openvino/backend.py
@@ -9,6 +9,13 @@ from vcap import (
     OPTION_TYPE,
     BaseStreamState)
 from vcap_utils import BaseOpenVINOBackend
+from vcap import __version__ as vcap_version
+def openvino2024_compatible():
+    from packaging import version
+    from vcap import __version__ as vcap_version
+    openvino2024_compatible_vcap_version = '0.3.8'
+    return version.parse(vcap_version) >= version.parse(openvino2024_compatible_vcap_version)
+__openvino2024__ = openvino2024_compatible()
 
 EMOTION_TYPES = ['neutral', 'happy', 'sad', 'surprise', 'anger']
 
@@ -24,14 +31,18 @@ class Backend(BaseOpenVINOBackend):
         input_dict, _ = self.prepare_inputs(crop)
         prediction = self.send_to_batch(input_dict).result()
 
-        prob_key = next(key for key in prediction.keys() if 'prob_emotion' in key.names)
-        prob_data = prediction[prob_key]
-        if isinstance(prob_data, np.ndarray):
-            emotion_id = prob_data.flatten().argmax()
-            emotion_score = float(prob_data.flatten()[emotion_id])
+        if __openvino2024__:
+            prob_key = next(key for key in prediction.keys() if 'prob_emotion' in key.names)
+            prob_data = prediction[prob_key]
+            if isinstance(prob_data, np.ndarray):
+                emotion_id = prob_data.flatten().argmax()
+                emotion_score = float(prob_data.flatten()[emotion_id])
+            else:
+                emotion_id = 0 if prob_data[0] > prob_data[1] else 1
+                emotion_score = float(prob_data[emotion_id])
         else:
-            emotion_id = 0 if prob_data[0] > prob_data[1] else 1
-            emotion_score = float(prob_data[emotion_id])
+            emotion_id = int(prediction["prob_emotion"].argmax())
+            emotion_score = float(prediction["prob_emotion"].flatten()[emotion_id])
 
         emotion = EMOTION_TYPES[emotion_id]
 
